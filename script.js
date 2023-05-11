8528b5289b479f1219bffcf136dea8e1574570f4
diegolimagomes <diegocontatodl@gmail.com>
2023-05-11 08:39:21 -0300

    primeiro Commit Starbucks

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let imagem = document.querySelector('.starbucks')
+let circulo = document.querySelector('.circulo')
+
+console.log(imagem);
+
+function trocaImagem(endereco) {
+  imagem.src = endereco
+}
+
+function trocaCor(cor) {
+  circulo.style.background = cor
+}
+
+
+
+
+
